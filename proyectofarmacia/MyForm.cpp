fd30796e978939efed47df67e271b6d066d269d4
Jose Valle <jose_vr_th98@hotmail.com>
2023-11-06 19:29:01 -0600

    Cambio Jodr

--- proyectofarmacia/MyForm.cpp
+++ proyectofarmacia/MyForm.cpp
@@ -1,2 +1,14 @@
 #include "MyForm.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+int main(array<String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    proyectofarmacia::MyForm form;
+    Application::Run(% form);
+    return 0;
+}
+
--- proyectofarmacia/MyForm.h
+++ proyectofarmacia/MyForm.h
@@ -49,12 +49,21 @@ namespace proyectofarmacia
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(282, 253);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
 	};
 }
