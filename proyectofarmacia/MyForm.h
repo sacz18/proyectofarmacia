62de01d7c43d6eaaf7057d65de3e7f4b3a643d25
Santiago Coppens <santi6yahoo.es@gmail.com>
2023-11-09 20:10:18 -0600

    ultima actualización

--- proyectofarmacia/MyForm.h
+++ proyectofarmacia/MyForm.h
@@ -1,4 +1,11 @@
 #pragma once
+#include "FormInventario.h"
+#include "FormMedicina.h"
+#include "Inventario.h"
+#include "Medicamento.h"
+#include "Proovedor.h"
+#include "FormProveedor.h"
+
 
 namespace proyectofarmacia 
 {
@@ -35,7 +42,13 @@ namespace proyectofarmacia
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ AgregarMed;
+	private: System::Windows::Forms::Button^ AgregarProv;
+	protected:
+
+
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
 	protected:
 
 	private:
@@ -51,25 +64,60 @@ namespace proyectofarmacia
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->AgregarMed = (gcnew System::Windows::Forms::Button());
+			this->AgregarProv = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
-			// button1
+			// AgregarMed
 			// 
-			this->button1->Location = System::Drawing::Point(245, 273);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(118, 23);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"Agregar medicina";
-			this->button1->UseVisualStyleBackColor = true;
+			this->AgregarMed->Location = System::Drawing::Point(245, 273);
+			this->AgregarMed->Name = L"AgregarMed";
+			this->AgregarMed->Size = System::Drawing::Size(118, 23);
+			this->AgregarMed->TabIndex = 0;
+			this->AgregarMed->Text = L"Agregar medicina";
+			this->AgregarMed->UseVisualStyleBackColor = true;
+			this->AgregarMed->Click += gcnew System::EventHandler(this, &MyForm::AgregarMed_Click);
+			// 
+			// AgregarProv
+			// 
+			this->AgregarProv->Location = System::Drawing::Point(119, 231);
+			this->AgregarProv->Name = L"AgregarProv";
+			this->AgregarProv->Size = System::Drawing::Size(75, 42);
+			this->AgregarProv->TabIndex = 1;
+			this->AgregarProv->Text = L"Agregar proveedor";
+			this->AgregarProv->UseVisualStyleBackColor = true;
+			this->AgregarProv->Click += gcnew System::EventHandler(this, &MyForm::AgregarProv_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(276, 199);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 40);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"Administrar Inventario";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(427, 272);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"Imprimir";
+			this->button4->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(635, 374);
-			this->Controls->Add(this->button1);
-			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->AgregarProv);
+			this->Controls->Add(this->AgregarMed);
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
@@ -79,5 +127,14 @@ namespace proyectofarmacia
 #pragma endregion
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void AgregarMed_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		FormMedicina^ form_medicina = gcnew FormMedicina();
+		form_medicina->Show();
+	}
+	private: System::Void AgregarProv_Click(System::Object^ sender, System::EventArgs^ e)
+	{
+		
+	}
+};
 }
