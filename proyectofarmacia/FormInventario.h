a0d01f92ba9cdbc2471669e33105a2b4b387453b
Santiago Coppens <santi6yahoo.es@gmail.com>
2023-11-10 18:47:07 -0600

    ultima act

--- proyectofarmacia/FormInventario.h
+++ proyectofarmacia/FormInventario.h
@@ -105,31 +105,31 @@ namespace proyectofarmacia {
 			// 
 			// preciocompra
 			// 
-			this->preciocompra->Location = System::Drawing::Point(138, 8);
+			this->preciocompra->Location = System::Drawing::Point(157, 8);
 			this->preciocompra->Name = L"preciocompra";
-			this->preciocompra->Size = System::Drawing::Size(165, 20);
+			this->preciocompra->Size = System::Drawing::Size(145, 20);
 			this->preciocompra->TabIndex = 0;
 			this->preciocompra->TextChanged += gcnew System::EventHandler(this, &FormInventario::preciocompra_TextChanged);
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(138, 46);
+			this->textBox2->Location = System::Drawing::Point(157, 46);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(165, 20);
+			this->textBox2->Size = System::Drawing::Size(146, 20);
 			this->textBox2->TabIndex = 1;
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(138, 80);
+			this->textBox3->Location = System::Drawing::Point(157, 80);
 			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(165, 20);
+			this->textBox3->Size = System::Drawing::Size(146, 20);
 			this->textBox3->TabIndex = 2;
 			// 
 			// textBox5
 			// 
-			this->textBox5->Location = System::Drawing::Point(138, 161);
+			this->textBox5->Location = System::Drawing::Point(157, 159);
 			this->textBox5->Name = L"textBox5";
-			this->textBox5->Size = System::Drawing::Size(165, 20);
+			this->textBox5->Size = System::Drawing::Size(145, 20);
 			this->textBox5->TabIndex = 4;
 			// 
 			// label1
@@ -137,11 +137,11 @@ namespace proyectofarmacia {
 			this->label1->AutoSize = true;
 			this->label1->BackColor = System::Drawing::Color::Bisque;
 			this->label1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Myanmar Text", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
+			this->label1->Font = (gcnew System::Drawing::Font(L"Kristen ITC", 8.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
 			this->label1->Location = System::Drawing::Point(12, 9);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(119, 25);
+			this->label1->Size = System::Drawing::Size(125, 18);
 			this->label1->TabIndex = 5;
 			this->label1->Text = L"Precio de compra:";
 			this->label1->Click += gcnew System::EventHandler(this, &FormInventario::label1_Click);
@@ -151,11 +151,11 @@ namespace proyectofarmacia {
 			this->label2->AutoSize = true;
 			this->label2->BackColor = System::Drawing::Color::Bisque;
 			this->label2->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Myanmar Text", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
+			this->label2->Font = (gcnew System::Drawing::Font(L"Kristen ITC", 8.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
 			this->label2->Location = System::Drawing::Point(12, 41);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(117, 25);
+			this->label2->Size = System::Drawing::Size(125, 18);
 			this->label2->TabIndex = 6;
 			this->label2->Text = L"Precio de venta:   ";
 			// 
@@ -164,11 +164,11 @@ namespace proyectofarmacia {
 			this->label3->AutoSize = true;
 			this->label3->BackColor = System::Drawing::Color::Bisque;
 			this->label3->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Myanmar Text", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
+			this->label3->Font = (gcnew System::Drawing::Font(L"Kristen ITC", 8.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
 			this->label3->Location = System::Drawing::Point(12, 80);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(118, 25);
+			this->label3->Size = System::Drawing::Size(119, 18);
 			this->label3->TabIndex = 7;
 			this->label3->Text = L"Unidades:            ";
 			// 
@@ -177,11 +177,11 @@ namespace proyectofarmacia {
 			this->label4->AutoSize = true;
 			this->label4->BackColor = System::Drawing::Color::Bisque;
 			this->label4->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
-			this->label4->Font = (gcnew System::Drawing::Font(L"Myanmar Text", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
+			this->label4->Font = (gcnew System::Drawing::Font(L"Kristen ITC", 8.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
 			this->label4->Location = System::Drawing::Point(12, 120);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(116, 25);
+			this->label4->Size = System::Drawing::Size(119, 18);
 			this->label4->TabIndex = 8;
 			this->label4->Text = L"Proveedor:          ";
 			// 
@@ -190,11 +190,11 @@ namespace proyectofarmacia {
 			this->label5->AutoSize = true;
 			this->label5->BackColor = System::Drawing::Color::Bisque;
 			this->label5->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
-			this->label5->Font = (gcnew System::Drawing::Font(L"Myanmar Text", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
+			this->label5->Font = (gcnew System::Drawing::Font(L"Kristen ITC", 8.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
 			this->label5->Location = System::Drawing::Point(12, 159);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(116, 22);
+			this->label5->Size = System::Drawing::Size(139, 18);
 			this->label5->TabIndex = 9;
 			this->label5->Text = L"Fecha de caducidad:";
 			this->label5->Click += gcnew System::EventHandler(this, &FormInventario::label5_Click);
@@ -202,87 +202,107 @@ namespace proyectofarmacia {
 			// comboBox1
 			// 
 			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(138, 120);
+			this->comboBox1->Location = System::Drawing::Point(157, 120);
 			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(165, 21);
+			this->comboBox1->Size = System::Drawing::Size(146, 21);
 			this->comboBox1->TabIndex = 10;
 			// 
 			// label6
 			// 
 			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(33, 220);
+			this->label6->BackColor = System::Drawing::Color::Bisque;
+			this->label6->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Kristen ITC", 8.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label6->Location = System::Drawing::Point(59, 237);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(36, 13);
+			this->label6->Size = System::Drawing::Size(48, 18);
 			this->label6->TabIndex = 20;
 			this->label6->Text = L"Dosis:";
 			// 
 			// label7
 			// 
 			this->label7->AutoSize = true;
-			this->label7->Location = System::Drawing::Point(21, 172);
+			this->label7->BackColor = System::Drawing::Color::Bisque;
+			this->label7->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->label7->Font = (gcnew System::Drawing::Font(L"Kristen ITC", 8.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label7->Location = System::Drawing::Point(29, 170);
 			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(92, 13);
+			this->label7->Size = System::Drawing::Size(78, 34);
 			this->label7->TabIndex = 19;
-			this->label7->Text = L"Principios activos:";
+			this->label7->Text = L"Principios \r\nactivos:";
 			// 
 			// label8
 			// 
 			this->label8->AutoSize = true;
-			this->label8->Location = System::Drawing::Point(44, 105);
+			this->label8->BackColor = System::Drawing::Color::Bisque;
+			this->label8->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->label8->Font = (gcnew System::Drawing::Font(L"Kristen ITC", 8.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label8->Location = System::Drawing::Point(14, 127);
 			this->label8->Name = L"label8";
-			this->label8->Size = System::Drawing::Size(69, 13);
+			this->label8->Size = System::Drawing::Size(97, 18);
 			this->label8->TabIndex = 18;
 			this->label8->Text = L"Clasificación:";
 			// 
 			// label9
 			// 
 			this->label9->AutoSize = true;
-			this->label9->Location = System::Drawing::Point(2, 51);
+			this->label9->BackColor = System::Drawing::Color::Bisque;
+			this->label9->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->label9->Font = (gcnew System::Drawing::Font(L"Kristen ITC", 8.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label9->Location = System::Drawing::Point(29, 71);
 			this->label9->Name = L"label9";
-			this->label9->Size = System::Drawing::Size(99, 13);
+			this->label9->Size = System::Drawing::Size(82, 34);
 			this->label9->TabIndex = 17;
-			this->label9->Text = L"Número de registro:";
+			this->label9->Text = L"Número de \r\nregistro:";
 			// 
 			// label10
 			// 
 			this->label10->AutoSize = true;
-			this->label10->Location = System::Drawing::Point(14, 8);
+			this->label10->BackColor = System::Drawing::Color::Bisque;
+			this->label10->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->label10->Font = (gcnew System::Drawing::Font(L"Kristen ITC", 8.25F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label10->Location = System::Drawing::Point(3, 22);
 			this->label10->Name = L"label10";
-			this->label10->Size = System::Drawing::Size(130, 13);
+			this->label10->Size = System::Drawing::Size(108, 34);
 			this->label10->TabIndex = 16;
-			this->label10->Text = L"Nombre del medicamento:";
+			this->label10->Text = L"Nombre del \r\nmedicamento:   \r\n";
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(75, 220);
+			this->textBox1->Location = System::Drawing::Point(128, 237);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(100, 20);
 			this->textBox1->TabIndex = 15;
 			// 
 			// textBox4
 			// 
-			this->textBox4->Location = System::Drawing::Point(119, 169);
+			this->textBox4->Location = System::Drawing::Point(128, 184);
 			this->textBox4->Name = L"textBox4";
 			this->textBox4->Size = System::Drawing::Size(100, 20);
 			this->textBox4->TabIndex = 14;
 			// 
 			// textBox6
 			// 
-			this->textBox6->Location = System::Drawing::Point(119, 98);
+			this->textBox6->Location = System::Drawing::Point(128, 125);
 			this->textBox6->Name = L"textBox6";
 			this->textBox6->Size = System::Drawing::Size(100, 20);
 			this->textBox6->TabIndex = 13;
 			// 
 			// textBox7
 			// 
-			this->textBox7->Location = System::Drawing::Point(107, 48);
+			this->textBox7->Location = System::Drawing::Point(128, 85);
 			this->textBox7->Name = L"textBox7";
 			this->textBox7->Size = System::Drawing::Size(100, 20);
 			this->textBox7->TabIndex = 12;
 			// 
 			// textBox8
 			// 
-			this->textBox8->Location = System::Drawing::Point(150, 5);
+			this->textBox8->Location = System::Drawing::Point(128, 36);
 			this->textBox8->Name = L"textBox8";
 			this->textBox8->Size = System::Drawing::Size(100, 20);
 			this->textBox8->TabIndex = 11;
@@ -290,7 +310,7 @@ namespace proyectofarmacia {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(522, 62);
+			this->button1->Location = System::Drawing::Point(171, 330);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 21;
@@ -310,7 +330,7 @@ namespace proyectofarmacia {
 			this->panel1->Controls->Add(this->label3);
 			this->panel1->Controls->Add(this->label4);
 			this->panel1->Controls->Add(this->label5);
-			this->panel1->Location = System::Drawing::Point(34, 221);
+			this->panel1->Location = System::Drawing::Point(33, 91);
 			this->panel1->Name = L"panel1";
 			this->panel1->Size = System::Drawing::Size(312, 193);
 			this->panel1->TabIndex = 22;
@@ -328,7 +348,7 @@ namespace proyectofarmacia {
 			this->panel2->Controls->Add(this->label8);
 			this->panel2->Controls->Add(this->label10);
 			this->panel2->Controls->Add(this->label9);
-			this->panel2->Location = System::Drawing::Point(368, 198);
+			this->panel2->Location = System::Drawing::Point(349, 91);
 			this->panel2->Name = L"panel2";
 			this->panel2->Size = System::Drawing::Size(269, 308);
 			this->panel2->TabIndex = 23;
@@ -339,7 +359,7 @@ namespace proyectofarmacia {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
-			this->ClientSize = System::Drawing::Size(649, 541);
+			this->ClientSize = System::Drawing::Size(649, 418);
 			this->Controls->Add(this->panel2);
 			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->button1);
